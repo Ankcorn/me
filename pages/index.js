0a131417dd02b0ca28db66fc7d0cabde6724773f
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-05-03 23:21:28 +0100

    Inital commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default () => (
+  <div className="flex w-screen justify-center p-16">
+    <div className="max-w-xs sm:max-w-2xl space-y-6">
+      <h1 className="text-3xl sm:text-5xl leading-tight">I write software and bring joy</h1>
+      <p className="text-lg font-medium">
+        I'm Thomas a software developer living and working in London.
+        I enjoy mentoring, coaching, making things as simple as possible,
+        and discussing ways we can craft software better.
+      </p>
+      <p className="text-lg font-medium">
+        I currently work at <a href="https://near.st" className="underline--magical">NearSt</a> building a data platform helping high street shops sell more locally.
+      </p>
+      <button className="bg-black rounded-lg py-3 px-5 text-white">Learn More</button>
+    </div>
+ 
+  </div>
+)
