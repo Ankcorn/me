b033bfca4411dae1f1ee6f58f9742eef5a314dce
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-05-04 21:41:22 +0100

    lets ship it

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 export default () => (
   <div className="flex w-screen justify-center p-16">
     <div className="max-w-xs sm:max-w-2xl space-y-6">
-      <h1 className="text-3xl sm:text-5xl leading-tight">I write software and bring joy</h1>
+      <h1 className="text-3xl sm:text-5xl leading-tight">Full Stack Developer, Serverless-First Advocate, and Software Coach</h1>
       <p className="text-lg font-medium">
         I'm Thomas a software developer living and working in London.
         I enjoy mentoring, coaching, making things as simple as possible,
         and discussing ways we can craft software better.
       </p>
       <p className="text-lg font-medium">
-        I currently work at <a href="https://near.st" className="underline--magical">NearSt</a> building a data platform helping high street shops sell more locally.
+        I currently work at <a href="https://near.st" className="underline--magical">NearSt</a> building a data platform to help high street shops sell more locally.
       </p>
-      <button className="bg-black rounded-lg py-3 px-5 text-white">Learn More</button>
+      <a className="bg-black rounded-lg py-3 px-5 my-2 text-white flex space-x-3 w-40" href="https://twitter.com/thomasankcorn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+        <span>Follow Me</span>
+      </a>
     </div>
  
   </div>
