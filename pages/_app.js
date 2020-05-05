be5d1b2da663fe494e7568a7c79cd37308270d50
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-05-05 06:33:34 +0100

    fix head

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Head from 'next/head'
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <title>My new cool app</title>
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        description={
+          "Systems Engineer, Software Coach and Serverless First Architect." +
+          "I do 1 on 1 coaching in react, nodejs, serverless, clean code, tdd and crafting software."
+        }
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
--- pages/_document.js
+++ pages/_document.js
@@ -6,15 +6,6 @@ class CustomDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          <title>Thomas Ankcorn</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-            description={
-              "Systems Engineer, Software Coach and Serverless First Architect." +
-              "I do 1 on 1 coaching in react, nodejs, serverless, clean code, tdd and crafting software."
-            }
-          />
           <link
             href="https://fonts.googleapis.com/css?family=Inter:500,600,700,800,900&display=swap'"
             rel="stylesheet"
