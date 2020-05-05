90b94b504ff422b96f07486130672377318bfe70
ThomasAnkcorn <thomasankcorn@gmail.com>
2020-05-05 07:12:29 +0100

    try fav again

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default () => (
         I currently work at <a href="https://near.st" className="underline--magical">NearSt</a> building a data platform to help high street shops sell more locally.
       </p>
       <a className="bg-black rounded-lg py-3 px-5 my-2 text-white flex space-x-3 w-40" href="https://twitter.com/thomasankcorn">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeWidth="round" stroke-linejoin="round" className="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
         <span>Follow Me</span>
       </a>
     </div>
